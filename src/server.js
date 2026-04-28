044fd8a8de4a3a82351a67e47291d00278f6f046
kaiorodriguess010-lgtm <kaiorodriguess010@gmail.com>
2026-04-27 22:11:44 -0300

    mvc-lista-de-usuario

--- src/server.js
+++ src/server.js
@@ -6,7 +6,24 @@ function callbackDaRaiz( request, response ) {
        response.send("AEROMOVEL BRASIL, SELVA, PANTANAL, MONTANHA, CAATINGA!!!!🚁")
 }
 
+
+function callbackListaUsuarios(request, response){
+
+
+    const listaUsuarios = [ {
+        id: 1,
+        name: "João"
+    }
+]
+
+
+    response.json( listaUsuarios)
+
+}
+
 app.get("/", callbackDaRaiz)
+
+app.get("/users", callbackListaUsuarios)
 
 app.listen(3000, () => {
     console.log("API rodando em http://localhost:3000")
